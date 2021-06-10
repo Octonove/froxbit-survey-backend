76dd5d84d7e69b7be3e90b70ee60d1c0be885578
drolegc <drolegc@gmail.com>
2021-06-10 20:15:54 -0300

    Init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dc8d2fee5de200be70e437e657a020e7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '84d44b37-80ba-4022-bf34-5cbad8aac923'
+};
